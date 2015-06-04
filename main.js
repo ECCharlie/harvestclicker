6284a4e263ef4e60780340571ddb43738620d069
Jesse Rhodes <l337rhodes@gmail.com>
2015-06-03 23:59:15 -0400

    Attempting to change clicks count up to countdown

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 var cropClicks = 0
+var neededClicks = 40
 
-function cropClick(number){
+/* function cropClick(number){
 	cropClicks = cropClicks + number;
 	if (cropClicks >= 10 && cropClicks < 20) {
 		document.getElementById("btn").style.color = "red";
@@ -12,6 +13,22 @@ function cropClick(number){
 		document.getElementById("btn").style.color = "orange";
 	}
 	else if (cropClicks >= 40) {
+		document.getElementById("btn").innerHTML = "Hello Charlie!";
+	}
+} */
+
+function cropClick(number){
+	neededClicks = cropClicks - number;
+	if (neededClicks <= 30 && neededClicks > 20) {
+		document.getElementById("btn").style.color = "red";
+	}
+	else if (neededClicks <= 20 && neededClicks > 10) {
+		document.getElementById("btn").style.color = "green";
+	}
+	else if (neededClicks >= 30 && neededClicks < 40) {
+		document.getElementById("btn").style.color = "orange";
+	}
+	else if (neededClicks >= 40) {
 		document.getElementById("btn").innerHTML = "Hello Charlie!";
 	}
 }
