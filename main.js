8563065d4228ded178bb478f5bf2aefa7fac232a
Jesse Rhodes <l337rhodes@gmail.com>
2015-06-09 00:18:56 -0400

    Created actual plot object
    Replaces old manual code, will need a lot of reworking. Needs draw
    feature and mass producing capabilities.

--- main.js
+++ main.js
@@ -1,13 +1,48 @@
-var cropClicks = 0
-var neededClicks = 40
-
 document.addEventListener('DOMContentLoaded',domloaded,false);
 function domloaded(){
 	var c = document.getElementById("myCanvas");
 	var ctx = c.getContext("2d");
 	ctx.fillStyle = "#006400";
 	ctx.fillRect(0,0,100,100);
+	var plot = new Plot(1);
 }
+
+function Plot (count) {
+	var that = this;
+	console.log("plot initialized");
+	this.count = count;
+	this.state = "0"; // 0 = super grassy, 1-3 = less grassy respectively, 4 = dirt.
+	var c = document.getElementById("myCanvas");
+	var ctx = c.getContext("2d");
+	this.clicks = 0;
+	this.checkState = function() {
+		if (this.state == 0) {
+			this.clicks += 1;
+			console.log("clicked");
+			if (this.clicks == 10) {
+				ctx.fillStyle = "#009900";
+				this.state = 1;
+				this.clicks = 0;
+			}
+		}
+		if (this.state == 1) {
+			this.clicks += 1;
+			console.log("clicked^2");
+			if (this.clicks == 10){
+				ctx.fillStyle = "#00cc00";
+				this.state = 2;
+				this.clicks = 0;
+			}
+		}
+		ctx.fillRect(0,0,100,100);
+	}
+	c.onclick = function() {
+		that.checkState();
+	}
+}
+
+
+
 
 /* function cropClick(number){
 	cropClicks = cropClicks + number;
@@ -25,7 +60,7 @@ function domloaded(){
 	}
 } */
 
-function cropClick(number){
+/*function cropClick(number){
 	var c = document.getElementById("myCanvas");
 	var ctx = c.getContext("2d");
 	neededClicks = neededClicks - number;
@@ -42,4 +77,4 @@ function cropClick(number){
 		ctx.fillStyle = "#573B0C";
 	}
 	ctx.fillRect(0,0,100,100);
-}
+}*/
